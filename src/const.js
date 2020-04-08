a6749d515f82c4ecbaf9b5b3220bfecd56e78e62
yoksel <yokselzok@gmail.com>
2020-04-08 19:51:13 +0300

    Add global consts

---
+++ src/const.js
@@ -0,0 +1,109 @@
+const POSTERS = [
+  'made-for-each-other.png',
+  'popeye-meets-sinbad.png',
+  'sagebrush-trail.jpg',
+  'santa-claus-conquers-the-martians.jpg',
+  'the-dance-of-life.jpg',
+  'the-great-flamarion.jpg',
+  'the-man-with-the-golden-arm.jpg',
+];
+
+const EMOJI = [
+  'angry.png',
+  'puke.png',
+  'sleeping.png',
+  'smile.png',
+];
+
+const TITLES = [
+  'The Dance of Life',
+  'Sagebrush Trail',
+  'The Man with the Golden Arm',
+  'Santa Claus Conquers the Martians',
+  'Popeye the Sailor Meets Sindbad ',
+  'The Man with the Golden Arm',
+  'The Great Flamarion',
+  'Santa Claus Conquers the Martians',
+  'Made for Each Other',
+];
+
+const SENTENCES = [
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  'Cras aliquet varius magna, non porta ligula feugiat eget. ',
+  'Fusce tristique felis at fermentum pharetra.',
+  'Aliquam id orci ut lectus varius viverra.',
+  'Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.',
+  'Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.',
+  'Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.',
+  'Sed sed nisi sed augue convallis suscipit in sed felis.',
+  'Aliquam erat volutpat.',
+  'Nunc fermentum tortor ac porta dapibus.',
+  'In rutrum ac purus sit amet tempus.',
+];
+
+const GENRES = [
+  'Musical',
+  'Western',
+  'Drama',
+  'Comedy',
+  'Cartoon',
+  'Mystery',
+];
+
+const NAMES = [
+  'Luciana Khan',
+  'Josh Cross',
+  'Yu Fuller',
+  'Larry Goodman',
+  'Nida Becker',
+  'Eshaal Carter',
+  'Zayn Rigby',
+  'Bill Davies',
+  'Robson Merrill',
+  'Dru Sharpe',
+];
+
+const AGE_RATINGS = {
+  'G': 'General audiences',
+  'PG': 'Parental guidance',
+  'PG-13': 'Parents strongly cautioned',
+  'R': 'Restricted',
+  'NC-17': 'No One 17 & Under Admitted',
+  'NR': 'Not Rated',
+};
+
+const COUNTRIES = [
+  'United States',
+  'France',
+  'China',
+  'India',
+  'United Kingdom',
+  'Nigeria',
+  'Egypt',
+  'Iran',
+  'Japan',
+  'Korea',
+  'Hong Kong',
+  'Turkey',
+  'Pakistan',
+  'Bangladesh',
+  'Indonesia',
+  'Trinidad and Tobago',
+  'Nepal',
+];
+
+const MAX_CARDS = 23;
+const MAX_CARDS_LOAD = 20;
+
+export {
+  POSTERS,
+  EMOJI,
+  TITLES,
+  SENTENCES,
+  GENRES,
+  NAMES,
+  AGE_RATINGS,
+  COUNTRIES,
+  MAX_CARDS,
+  MAX_CARDS_LOAD
+};
