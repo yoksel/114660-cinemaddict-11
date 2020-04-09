439f24ffc1125c07449e64ca211a8147506eb270
yoksel <yokselzok@gmail.com>
2020-04-09 09:24:14 +0300

    Rename menu to filter

--- src/components/filter.js
+++ src/components/filter.js
@@ -1,4 +1,4 @@
-export default class Menu {
+export default class Filter {
   getTmpl() {
     return (
       `<nav class="main-navigation">
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { MAX_CARDS, MAX_CARDS_LOAD } from './const.js';
 
 import Profile from './components/profile';
-import Menu from './components/menu';
+import Filter from './components/filter';
 import Sort from './components/sort';
 import Films from './components/films';
 import Details from './components/details';
@@ -16,7 +16,7 @@ const siteMainElem = document.querySelector(`.main`);
 const siteFooterElem = document.querySelector(`.footer`);
 
 const profile = new Profile();
-const menu = new Menu();
+const filter = new Filter();
 const sort = new Sort();
 const films = new Films(cardsData);
 const details = new Details(cardsData[0]);
@@ -28,7 +28,7 @@ const render = (container, template) => {
 
 render(siteHeaderElem, profile.getTmpl());
 
-render(siteMainElem, menu.getTmpl());
+render(siteMainElem, filter.getTmpl());
 render(siteMainElem, sort.getTmpl());
 render(siteMainElem, films.getTmpl());
 
