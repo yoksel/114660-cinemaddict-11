dc75b31318f122c379c1a7407d6d7ca5cf72ef8f
yoksel <yokselzok@gmail.com>
2020-04-12 17:57:22 +0300

    Add markup generation to Sort component

--- src/components/sort.js
+++ src/components/sort.js
@@ -1,11 +1,35 @@
 import {createElement} from '../helpers';
 
 export default class Sort {
+  constructor(currentSort) {
+    this.currentSort = currentSort || `default`;
+
+    this.sections = [
+      `default`,
+      `date`,
+      `rating`
+    ];
+  }
+
+  getItems() {
+    return this.sections.reduce((prev, item) => {
+      let className = `sort__button`;
+
+      if (item === this.currentSort) {
+        className += ` ${className}--active`;
+      }
+
+      return (
+        `${prev}<li>
+          <a href="#" class="${className}">Sort by ${item}</a>
+        </li>`
+      );
+    }, ``);
+  }
+
   getElement() {
     const markup = `<ul class="sort">
-      <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-      <li><a href="#" class="sort__button">Sort by date</a></li>
-      <li><a href="#" class="sort__button">Sort by rating</a></li>
+      ${this.getItems()}
     </ul>`;
 
     return createElement(markup);
