c78557dfc3c65855f7cee89bff0c334521e090c2
yoksel <yokselzok@gmail.com>
2020-04-23 22:46:56 +0300

    Update Filter component to be used in PageController

--- src/components/filter.js
+++ src/components/filter.js
@@ -1,15 +1,22 @@
 import AbstractComponent from './abstract-component';
 
+const classes = {
+  default: `main-navigation__item`,
+  active: `main-navigation__item--active`
+};
+
 export default class Filter extends AbstractComponent {
   constructor({cardsData, currentFilter}) {
     super();
 
     this._data = cardsData;
-    this._currentFilter = currentFilter || `all`;
+    this._defaultFilter = `all`;
+    this._currentFilter = currentFilter || this._defaultFilter;
 
     this._sections = [
       {
         id: `all`,
+        key: `all`,
         name: `All movies`
       },
       {
@@ -30,14 +37,60 @@ export default class Filter extends AbstractComponent {
     ];
   }
 
+  reset() {
+    if (this._currentFilter === this._defaultFilter) {
+      return;
+    }
+
+    this._currentFilter = this._defaultFilter;
+    this._currentControl.classList.remove(classes.active);
+    this._currentControl = this.getElement().querySelector(`.${classes.default}--all`);
+    this._currentControl.classList.add(classes.active);
+  }
+
+  _createHandler(handler) {
+    return (event) => {
+      const control = event.target.closest(`.${classes.default}`);
+      event.preventDefault();
+
+      if (!control) {
+        return;
+      }
+
+      const {filterProp} = control.dataset;
+
+      if (filterProp === this._currentFilter) {
+        return;
+      }
+
+      if (!this._currentControl) {
+        this._currentControl = this.getElement().querySelector(`.${classes.active}`);
+      }
+
+      this._currentControl.classList.remove(classes.active);
+      control.classList.add(classes.active);
+
+      this._currentControl = control;
+      this._currentFilter = filterProp;
+      handler(filterProp);
+    };
+  }
+
+  setClickHandler(handler) {
+    this._clickHandler = this._createHandler(handler);
+
+    this.getElement().addEventListener(`click`, this._clickHandler);
+  }
+
   _getItems() {
     return this._sections.reduce((prev, section) => {
       const {id, key, name} = section;
       let counter = 0;
       let counterMarkup = ``;
-      let className = `main-navigation__item`;
+      let className = classes.default;
+      className += ` ${classes.default}--${id}`;
 
-      if (key) {
+      if (key !== `all`) {
         counter = this._data.filter((item) => item[key]).length;
         counterMarkup = `<span class="main-navigation__item-count">
           ${counter}
@@ -45,11 +98,11 @@ export default class Filter extends AbstractComponent {
       }
 
       if (id === this._currentFilter) {
-        className += ` ${className}--active`;
+        className += ` ${classes.active}`;
       }
 
       return (
-        `${prev} <a href="#${id}" class="${className}">
+        `${prev} <a href="#${id}" class="${className}" data-filter-prop="${key}">
           ${name} ${counterMarkup}
         </a>`
       );
