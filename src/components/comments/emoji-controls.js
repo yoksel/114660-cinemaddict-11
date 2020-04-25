eb5d0e4755e9ae453b4f00f0fb302f35ba8e55c3
yoksel <yokselzok@gmail.com>
2020-04-25 12:22:15 +0300

    Add setClickHandler() method to EmojiControls component

--- src/components/comments/emoji-controls.js
+++ src/components/comments/emoji-controls.js
@@ -2,6 +2,23 @@ import AbstractComponent from '../abstract-component';
 import {EMOJIS} from '../../constants';
 
 export default class EmojiControls extends AbstractComponent {
+  _getClickHandler(handler) {
+    return () => {
+      const control = event.target.closest(`.film-details__emoji-item`);
+
+      if (!control) {
+        return;
+      }
+
+      handler(control.value);
+    };
+  }
+
+  setClickHandler(handler) {
+    const clickHandler = this._getClickHandler(handler);
+    this.getElement().addEventListener(`click`, clickHandler);
+  }
+
   _getEmojiControl(name) {
     return (
       `<input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-${name}" value="${name}">
