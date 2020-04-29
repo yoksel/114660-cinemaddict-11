b5c4c6441f5caa952cea46fadd70fbba3918592c
yoksel <yokselzok@gmail.com>
2020-04-29 14:31:06 +0300

    Tiny refactoring in Filter component

--- src/components/filter.js
+++ src/components/filter.js
@@ -29,9 +29,9 @@ export default class Filter extends AbstractComponent {
         return;
       }
 
-      const {filterProp} = control.dataset;
+      const {filterType} = control.dataset;
 
-      if (filterProp === this._currentFilter) {
+      if (filterType === this._currentFilter) {
         return;
       }
 
@@ -43,8 +43,8 @@ export default class Filter extends AbstractComponent {
       control.classList.add(classes.active);
 
       this._currentControl = control;
-      this._currentFilter = filterProp;
-      handler(filterProp);
+      this._currentFilter = filterType;
+      handler(filterType);
     };
   }
 
@@ -55,25 +55,25 @@ export default class Filter extends AbstractComponent {
   }
 
   _getItems() {
-    return Object.entries(FILTERS).reduce((prev, [id, {name}]) => {
+    return Object.entries(FILTERS).reduce((prev, [type, {name}]) => {
       let counter = 0;
       let counterMarkup = ``;
       let className = classes.default;
-      className += ` ${classes.default}--${id}`;
+      className += ` ${classes.default}--${type}`;
 
-      if (id !== FilterType.ALL) {
-        counter = getFilmsByFilter(this._films, id).length;
+      if (type !== FilterType.ALL) {
+        counter = getFilmsByFilter(this._films, type).length;
         counterMarkup = `<span class="main-navigation__item-count">
           ${counter}
         </span>`;
       }
 
-      if (id === this._currentFilter) {
+      if (type === this._currentFilter) {
         className += ` ${classes.active}`;
       }
 
       return (
-        `${prev} <a href="#${id}" class="${className}" data-filter-prop="${id}">
+        `${prev} <a href="#${type}" class="${className}" data-filter-type="${type}">
           ${name} ${counterMarkup}
         </a>`
       );
