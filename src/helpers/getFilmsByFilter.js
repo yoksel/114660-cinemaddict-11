68daf93f44a7733e63586e9d61e8ae33257113ab
yoksel <yokselzok@gmail.com>
2020-05-02 15:36:58 +0300

    Beautify switch in getFilmsByFilter()

--- src/helpers/getFilmsByFilter.js
+++ src/helpers/getFilmsByFilter.js
@@ -4,11 +4,7 @@ export const getFilmsByFilter = (films, filterType) => {
   switch (filterType) {
     case FilterType.ALL:
       return films;
-    case FilterType.WATCHLIST:
-    case FilterType.HISTORY:
-    case FilterType.FAVORITES:
+    default:
       return films.filter((item) => item[FILTERS[filterType].prop]);
   }
-
-  return films;
 };
