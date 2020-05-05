4f0758a4cd151bba36bc9cae282db452d0b5c18a
yoksel <yokselzok@gmail.com>
2020-05-05 11:41:33 +0300

    Update main to use new controllers & User model

--- src/main.js
+++ src/main.js
@@ -1,36 +1,64 @@
 import FilmsModel from './models/films';
+import UserModel from './models/user';
 
+import ProfileController from './controllers/profile';
 import FilterController from './controllers/filter';
 import SortController from './controllers/sort';
 import PageController from './controllers/page';
+import UserStatsController from './controllers/user-stats';
 
-import Profile from './components/profile';
 import FilmsTotal from './components/films-total';
 
 import {renderElement} from './helpers';
+
+import {FilterType, SortType} from './constants';
 
 import {TOTAL_FILMS} from './mocks/constants';
 import {getCardsData} from './mocks/cards';
 import {getUserData} from './mocks/user';
 
+const userData = getUserData();
+
 const filmsModel = new FilmsModel();
 filmsModel.setFilms(getCardsData(TOTAL_FILMS));
 
-const cardsData = filmsModel.getFilms();
-const userData = getUserData(cardsData);
+const userModel = new UserModel(filmsModel);
+userModel.setUser(userData);
 
 const siteHeaderElem = document.querySelector(`.header`);
 const siteMainElem = document.querySelector(`.main`);
 const filmsTotalElem = document.querySelector(`.footer__statistics`);
 
+const profileController = new ProfileController(siteHeaderElem, filmsModel, userModel);
 const filterController = new FilterController(siteMainElem, filmsModel);
 const sortController = new SortController(siteMainElem, filmsModel);
 const pageController = new PageController(siteMainElem, filmsModel);
-const profile = new Profile(userData);
 const filmsTotal = new FilmsTotal(filmsModel.getFilmsQuantity());
+const userStatsContoller = new UserStatsController(siteMainElem, filmsModel, userModel);
 
-renderElement(siteHeaderElem, profile);
+const switchToFilms = () => {
+  sortController.show();
+  pageController.show();
+  userStatsContoller.hide();
+};
+
+const switchToStats = () => {
+  filmsModel.setFilterType(FilterType.ALL);
+  filmsModel.setSortType(SortType.DEFAULT);
+
+  sortController.hide();
+  pageController.hide();
+  userStatsContoller.show();
+};
+
+profileController.render();
 filterController.render();
 sortController.render();
 pageController.render();
+userStatsContoller.render();
+userStatsContoller.hide();
+
+filterController.setStatsClickHandler(switchToStats);
+filterController.setFilterItemClickHandler(switchToFilms);
+
 renderElement(filmsTotalElem, filmsTotal);
