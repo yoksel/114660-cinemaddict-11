12e6a743b08ba90ad37ebf403bd191c6724c2ede
yoksel <yokselzok@gmail.com>
2020-05-11 19:33:38 +0300

    Add + to age rating

--- src/components/details/poster.js
+++ src/components/details/poster.js
@@ -1,13 +1,11 @@
 import AbstractComponent from '../abstract-component';
 
-import {AGE_RATINGS} from '../../constants';
-
 export default class Poster extends AbstractComponent {
   constructor({poster, ageRating}) {
     super();
 
     this._poster = poster;
-    this._ageRating = ageRating;
+    this._ageRating = ageRating ? `${ageRating}+` : ``;
   }
 
   _getTmpl() {
@@ -15,7 +13,7 @@ export default class Poster extends AbstractComponent {
       `<div class="film-details__poster">
         <img class="film-details__poster-img" src="./${this._poster}" alt="">
 
-        <p class="film-details__age" title="${AGE_RATINGS[this._ageRating]}">${this._ageRating}</p>
+        <p class="film-details__age">${this._ageRating}</p>
       </div>`
     );
   }
