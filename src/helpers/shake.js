450b5b5b6db1d44524c5668e73cb592adf36e2ee
yoksel <yokselzok@gmail.com>
2020-05-15 15:01:49 +0300

    Make shaker shorter

--- src/helpers/shake.js
+++ src/helpers/shake.js
@@ -1,16 +1,10 @@
 import {ClassName} from '../constants';
 
-const getAnimationEndHandler = (container) => {
+export const shake = (container) => {
   const handler = () => {
     container.classList.remove(ClassName.SHAKE);
     container.removeEventListener(`animationend`, handler);
   };
-
-  return handler;
-};
-
-export const shake = (container) => {
-  const handler = getAnimationEndHandler(container);
 
   container.classList.add(ClassName.SHAKE);
   container.addEventListener(`animationend`, handler);
